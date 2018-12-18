1c8e096c9122983e0bd791318569eb6caaf4001d
qijunyao <qijunyao@xiaohongshu.com>
2018-12-18 20:36:16 +0800

    add all

--- elasticsearch/fabfile.py
+++ elasticsearch/fabfile.py
@@ -12,6 +12,26 @@ from fabric import task
 
 
 @task
+def appendBashProfile(ctx):
+    """
+    添加 es 原型上下文配置
+    :param ctx:
+    :return:
+    """
+    print 'bash profile content'
+
+
+@task
 def printHostname(ctx):
+    """
+    测试链接成功数据 hostname
+    :param ctx:
+    :return:
+    """
     result = ctx.run('hostname')
     print result
+
+
+@task
+def whoami(ctx):
+    print ctx.run('whoami')
