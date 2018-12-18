10a62752402b7d043b7fb0b626e5872905fe30df
qijunyao <qijunyao@xiaohongshu.com>
2018-12-19 00:27:14 +0800

    init file load

--- elasticsearch/elasticsearch_cluster_build.py
+++ elasticsearch/elasticsearch_cluster_build.py
@@ -7,5 +7,18 @@ __mail__ = 'yaoqijunmail@foxmail.com'
 '''
 description: service 集群group 构建es 集群方式
 '''
+from fabric import SerialGroup
+
+file_load_path = '/data/deploy/cellar'
+deploy_hosts = ['sns-es6-node07', 'sns-es6-node08', 'sns-es6-node09', 'sns-es6-node10', 'sns-es6-node11']
 
 
+if __name__ == '__main__':
+    print 'start to config elasticsearch cluster'
+    ctx = SerialGroup(*DEPLOY_HOSTS)
+    ctx.run('mkdir -r %s' % file_load_path)
+    ctx.run('cd %s' % file_load_path)
+    ctx.run('wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.2.tar.gz'
+            ' -e use_proxy=yes -e http_proxy=rec-httpproxy01:3128')
+    # init cluster content
+    print 'end to config elasticsearch cluster'
