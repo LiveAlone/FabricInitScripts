b5b5b4b1e772309427ebb2638ea2d2a8c75ba668
qijunyao <qijunyao@xiaohongshu.com>
2018-12-19 21:06:05 +0800

    pwd

--- elasticsearch/elasticsearch_cluster_build.py
+++ elasticsearch/elasticsearch_cluster_build.py
@@ -16,9 +16,10 @@ deploy_hosts = ['sns-es6-node07', 'sns-es6-node08', 'sns-es6-node09', 'sns-es6-n
 if __name__ == '__main__':
     print 'start to config elasticsearch cluster'
     ctx = SerialGroup(*deploy_hosts)
-    ctx.run('mkdir -p %s' % file_load_path)
+    # ctx.run('mkdir -p %s' % file_load_path)
     ctx.run('cd %s' % file_load_path)
-    ctx.run('wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.2.tar.gz'
-            ' -e use_proxy=yes -e http_proxy=rec-httpproxy01:3128')
+    ctx.run('pwd')
+    # ctx.run('wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.2.tar.gz'
+    #         ' -e use_proxy=yes -e http_proxy=rec-httpproxy01:3128')
     # init cluster content
     print 'end to config elasticsearch cluster'
