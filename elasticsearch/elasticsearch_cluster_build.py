fdb9825f6968d41e1482dddccc52315a8f7fde9d
qijunyao <qijunyao@xiaohongshu.com>
2018-12-19 21:16:35 +0800

    add all

--- elasticsearch/elasticsearch_cluster_build.py
+++ elasticsearch/elasticsearch_cluster_build.py
@@ -17,9 +17,7 @@ if __name__ == '__main__':
     print 'start to config elasticsearch cluster'
     ctx = SerialGroup(*deploy_hosts)
     # ctx.run('mkdir -p %s' % file_load_path)
-    ctx.cd(file_load_path)
-    # ctx.run('cd %s' % file_load_path)
-    ctx.run('pwd')
+    ctx.run('cd %s && pwd' % file_load_path)
     # ctx.run('wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.2.tar.gz'
     #         ' -e use_proxy=yes -e http_proxy=rec-httpproxy01:3128')
     # init cluster content
